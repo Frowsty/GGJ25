9e455c577785e16680e39a3a9d34b6ab742b72a6
Frowsty <daniel.barbis@frostys.tech>
2025-01-24 22:13:10 +0100

    Player movement

--- GGJ25/Assets/Project/Scripts/Player/PlayerMovement.cs
+++ GGJ25/Assets/Project/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,8 +5,18 @@ public class PlayerMovement : MonoBehaviour, IPlayerComponent
 {
     private Vector2 direction = Vector2.zero;
 
+    private Rigidbody2D rb2d;
+    private float speed = 10f;
+
+    private void Start()
+    {
+        if (rb2d == null)
+            rb2d = GetComponent<Rigidbody2D>();
+    }
+
     public void UpdateComponent()
     {
+        rb2d.linearVelocity = direction * speed;
     }
 
     private void OnMove(InputValue value)
