819e808af7c65652baceffa4c41cb2a3e50f1b92
Frowsty <daniel.barbis@frostys.tech>
2025-01-25 00:31:41 +0100

    Re-added attacking, fixed bubble prefab

--- GGJ25/Assets/Project/Scripts/Player/PlayerAttack.cs
+++ GGJ25/Assets/Project/Scripts/Player/PlayerAttack.cs
@@ -1,15 +1,34 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour, IPlayerComponent
 {
+    [SerializeField]
+    private BubbleMovement bubblePrefab;
+
+    public List<BubbleMovement> bubbles;
+
     public void UpdateComponent()
     {
-        
+        for (int i = bubbles.Count - 1; i >= 0; i--)
+        {
+            bubbles[i].UpdateBubble();
+            if (bubbles[i].shouldDestroy)
+            {
+                Destroy(bubbles[i].gameObject);
+                bubbles.RemoveAt(i);
+            }
+        }
     }
 
     private void OnAttack(InputValue value)
     {
-        Debug.Log("We're attacking");
+        var bubble = Instantiate(bubblePrefab, transform.position, Quaternion.identity);
+        Vector3 direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+        direction.Normalize();
+        bubble.direction = direction; 
+        bubble.spawnTime = Time.time;
+        bubbles.Add(bubble);
     }
 }
