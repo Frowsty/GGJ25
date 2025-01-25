857189fe893166af837e2eae5a7e420b8b75c141
jady17500 <144691322+jady17500@users.noreply.github.com>
2025-01-26 00:19:52 +0100

    Minor Fix

--- GGJ25/Assets/Project/Scripts/Enemy/Shell.cs
+++ GGJ25/Assets/Project/Scripts/Enemy/Shell.cs
@@ -1,7 +1,21 @@
+using System;
 using UnityEngine;
+using DG.Tweening;
 
 public class Shell : MonoBehaviour
 {
+    public float shellOffset;
+    public float shellSpeed;
+
+    public float startY;
+    
+    private void Awake()
+    {
+        startY = transform.position.y;
+        transform.DOMoveY(startY+shellOffset, shellSpeed).SetLoops(-1, LoopType.Yoyo);
+ 
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
